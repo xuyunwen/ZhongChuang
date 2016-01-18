361ab63161198e24b7722b2f393e8f3c3f227c6c
wen <xuyunwen1992@qq.com>
2016-01-19 01:38:22 +0800

    Create Controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   include SessionsHelper
+  include UsersHelper
 
   # Confirms a logged-in user.
   def logged_in_user
